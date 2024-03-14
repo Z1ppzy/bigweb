c77975b661ecddf33c0a4848e7b37c437f5068e2
Z1ppzy <mietofff@gmail.com>
2024-03-15 02:22:55 +0300

    fixed footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,13 +3,13 @@ import { FaDiscord, FaVk, FaTelegramPlane } from 'react-icons/fa';
 export default function Footer() {
   return (
     <footer>
-      <div className='w-full h-20 bg-footer flex flex-row justify-center'>
-        <div className='flex flex-col items-center'>
+      <div className='w-full bg-footer flex flex-row justify-center '>
+        <div className='flex flex-col items-center py-5'>
           <p className='font-bold mt-6 text-white'>
             © 2024 HeavenlyWeiner Все права защищены.
           </p>
           <div>
-            <div className='flex flex-row gap-2 text-xl text-white'>
+            <div className='flex flex-row gap-2 text-xl text-white '>
               <a href='https://discord.gg/hBzsDYcGZg'><FaDiscord className='cursor-pointer hover:text-purple-500' /></a>
               <a href='https://vk.com/heavenlyweiner'><FaVk className='cursor-pointer hover:text-purple-500' /></a>
               <a href='https://t.me/heavenlyweiner'><FaTelegramPlane className='cursor-pointer hover:text-purple-500' /></a>
