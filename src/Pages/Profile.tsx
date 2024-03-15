6c9b3dae51d8949d6583f1179f083e164e1bb5a8
Z1ppzy <mietofff@gmail.com>
2024-03-15 19:48:01 +0300

    add button in Profile page

--- src/Pages/Profile.tsx
+++ src/Pages/Profile.tsx
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router-dom';
 import 'react-toastify/dist/ReactToastify.css';
 import { Skeleton } from '@/components/ui/skeleton';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
-import { format } from "@formkit/tempo";
+import { format } from '@formkit/tempo';
 
 axios.defaults.withCredentials = true;
 axios.defaults.withXSRFToken = true;
@@ -32,7 +32,6 @@ export default function Profile() {
     fetchUser();
   }, []);
 
-
   return (
     <>
       <div>
@@ -43,43 +42,36 @@ export default function Profile() {
           <div className=''>
             <div className='py-5 px-8 text-left'>
               {user && (
-                <div className='flex flex-row'>
+                <div className='flex flex-col items-center md:flex-row '>
                   <Avatar>
-                    <AvatarImage
-                      src='12231231.jpg'
-                      alt='None'
-                    />
+                    <AvatarImage src='12231231.jpg' alt='None' />
                     <AvatarFallback>None</AvatarFallback>
                   </Avatar>
                   <div className='flex flex-col pl-6 text-md text-someblack text-lg'>
-                  <p>Почта: {user.email}</p>
-                  <p>Роль:{user.role}</p>
-                  <p>Аккаунт создан: {format(user.created_at, {date: 'medium'})}</p>
+                    <p>Почта: {user.email}</p>
+                    <p>Роль:{user.role}</p>
+                    <p>
+                      Аккаунт создан:{' '}
+                      {format(user.created_at, { date: 'medium' })}
+                    </p>
                   </div>
-
                 </div>
               )}
               {!user && <Skeleton className='h-[80px] w-[250px] rounded-xl' />}
             </div>
-            <div className='flex flex-row'>Смена пароля Активировать промокод</div>
-          </div>
-          <div className='mx-10 '>
-            <div className='bg-gradient-to-r from-sky-500 to-indigo-500 py-5 px-5 rounded-md'>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus
-              repellat perspiciatis numquam iure autem unde quisquam, adipisci
-              voluptatum dicta rem rerum qui, officia, non iusto reprehenderit
-              repellendus et! Quisquam libero itaque commodi natus odio,
-              voluptatum rerum minus dolorem tempore hic temporibus beatae
-              officia suscipit saepe architecto corrupti dolorum. Numquam,
-              illum.
-            </div>
-            <div className='bg-slate-100 py-5 px-5 mt-5 rounded-md'>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. In
-              fugiat corporis hic accusantium, porro dolor, exercitationem velit
-              rerum impedit odio nihil expedita corrupti temporibus error minus,
-              voluptatum odit nostrum unde sint. Laborum, porro asperiores esse
-              sit perferendis, consequuntur doloremque ad aspernatur debitis
-              accusantium iure dolorum voluptatibus, minus rem ab sint.
+            <div className='px-6'>
+              <button className='text-white w-fit bg-black px-2 py-1 rounded-md m-0.5 font-medium'>
+                Смена пароля
+              </button>
+              <button className='text-white w-fit bg-black px-2 py-1 rounded-md m-0.5 font-medium'>
+                Активация промокода
+              </button>
+              <button className='text-white w-fit bg-black px-2 py-1 rounded-md m-0.5 font-medium'>
+                Подтверждение почты
+              </button>
+              <button className='text-white w-fit bg-black px-2 py-1 rounded-md m-0.5 font-medium'>
+                Двухэтапная аутентификация
+              </button>
             </div>
           </div>
         </div>
