12cae4ec3aff74ccc93c011c93c2f191688d050d
Z1ppzy <mietofff@gmail.com>
2024-03-19 01:42:31 +0300

    fixed

--- src/components/ThemeSwitch.tsx
+++ src/components/ThemeSwitch.tsx
@@ -7,7 +7,7 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
-import { useTheme } from "@/components/Theme-Provider"
+import { useTheme } from "@/components/theme-provider"
 
 export function ThemeSwitch() {
   const { setTheme } = useTheme()
--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { RouterProvider } from 'react-router-dom';
 import router from './Routes';
-import { ThemeProvider } from '@/components/Theme-Provider';
+import { ThemeProvider } from '@/components/theme-provider';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
