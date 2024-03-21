d7c96f28e15aca326056ab5d310b2497ddcbde1a
Z1ppzy <mietofff@gmail.com>
2024-03-21 16:21:51 +0300

    added a check for button authorization

--- src/components/RegisterButton.tsx
+++ src/components/RegisterButton.tsx
@@ -1,6 +1,19 @@
 import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react'
 
 export default function RegisterButton() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    // Проверяем, есть ли токен авторизации в localStorage
+    const token = localStorage.getItem('authToken');
+    setIsLoggedIn(!!token);
+  }, []);
+
+  // Не отображаем кнопку "Войти", если пользователь уже авторизован
+  if (isLoggedIn) {
+    return null;
+  }
   return (
     <Link
       to='/register'
