7724ec977920a978bf3286cd706b3dd8585b9d8d
Z1ppzy <mietofff@gmail.com>
2024-03-31 03:13:44 +0300

    add new style logic

--- src/components/SuggestionBox.tsx
+++ src/components/SuggestionBox.tsx
@@ -6,7 +6,7 @@ export default function SuggestionBox() {
     <div className='w-full h-fit text-center p-6'>
       <div className='gap-3 flex flex-col items-center'>   
         <h1 className='font-bold text-xl'>Предложить идею по улучшению сервера</h1>
-        <p className='font-light md:font-extralight text-sm mt-0'>Идея должна быть адекватной и правильно выраженной!</p>
+        <p className='font-light md:dark:font-extralight text-sm mt-0 '>Идея должна быть адекватной и правильно выраженной!</p>
         <Textarea className='max-w-xl' placeholder='Снять зипзи с админа' />
         <Button className='w-fit'>Отправить</Button>
       </div>
