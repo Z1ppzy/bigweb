564a40440a60707d22d9ed1fe4d357663f0c4d41
Z1ppzy <mietofff@gmail.com>
2024-03-31 03:02:45 +0300

    change input in SuggestionBox

--- src/components/SuggestionBox.tsx
+++ src/components/SuggestionBox.tsx
@@ -1,13 +1,14 @@
 import { Button } from './ui/button';
-import { Input } from './ui/input';
+import { Textarea } from './ui/textarea';
 
 export default function SuggestionBox() {
   return (
     <div className='w-full h-fit text-center p-6'>
-      <div className=' gap-4 flex flex-col'>
+      <div className='gap-3 flex flex-col items-center'>   
         <h1 className='font-bold text-xl'>Предложить идею по улучшению сервера</h1>
-        <Input type='text'className='' />
-        <Button>Отправить</Button>
+        <p className='font-light md:font-extralight text-sm mt-0'>Идея должна быть адекватной и правильно выраженной!</p>
+        <Textarea className='max-w-xl' placeholder='Снять зипзи с админа' />
+        <Button className='w-fit'>Отправить</Button>
       </div>
     </div>
   );
