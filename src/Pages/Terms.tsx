9390b4bdcf079a98f4b758d0428f037d06ba334c
Z1ppzy <mietofff@gmail.com>
2024-04-03 21:44:21 +0300

    fixed style terms page

--- src/Pages/Terms.tsx
+++ src/Pages/Terms.tsx
@@ -2,14 +2,14 @@ import { HiAcademicCap } from "react-icons/hi2";
 import { Link } from 'react-router-dom';
 export default function Terms() {
   return (
-    <div className='mx-6 mt-20 md:mx-48 h-screen'>
+    <div className='px-6 mt-32 md:mx-48 h-screen'>
       <div className='flex flex-col items-center'>
-        <div className='flex flex-col text-4xl hover:text-purple-800'><Link to='/'><HiAcademicCap /></Link></div>
+        <div className='flex flex-col text-[80px] hover:text-purple-800'><Link to='/'><HiAcademicCap /></Link></div>
         <div className='flex flex-col'><h1 className='font-bold text-2xl transform transition-transform hover:scale-110'>Условия использования</h1></div>
       </div>
-      <div className='md:mt-8'>
-        <p className='font-semibold text-center'>
-          Вы соглашаетесь быть связанными нашими <Link to='/rules' className='hover:text-purple-800 hover:cursor-pointer'><strong>правилами</strong></Link> сайта и любыми
+      <div className='mt-2'>
+        <p className='font-light text-center'>
+          Вы соглашаетесь быть связанными нашими <Link to='/rules' className='hover:text-purple-800 hover:cursor-pointer font-medium'><strong>правилами</strong></Link> сайта и любыми
           законами, которые могут применяться к этому веб-сайту и вашему
           участию. Администрация сайта имеет право в любое время прекратить вашу
           учетную запись, удалить любое содержимое, которое вы могли разместить,
