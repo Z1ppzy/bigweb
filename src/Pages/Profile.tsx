f447ceae3ae152d1fcaf5b54dee01846e766d997
Z1ppzy <mietofff@gmail.com>
2024-04-05 13:55:34 +0300

    Changed style fot SuggestionBox

--- src/Pages/Profile.tsx
+++ src/Pages/Profile.tsx
@@ -11,7 +11,6 @@ import Greeting from '@/components/Greeting';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-
 axios.defaults.withCredentials = true;
 axios.defaults.withXSRFToken = true;
 
@@ -38,15 +37,18 @@ export default function Profile() {
       formData.append('avatar', selectedFile);
 
       try {
-        const response = await axios.post('http://localhost:8000/api/avatar/upload', formData);
+        const response = await axios.post(
+          'http://localhost:8000/api/avatar/upload',
+          formData
+        );
         console.log(response.data);
-        toast.success("Аватар успешно обновлен!"); 
+        toast.success('Аватар успешно обновлен!');
       } catch (error) {
         console.error(error);
-        toast.error("Ошибка при обновлении аватара."); 
+        toast.error('Ошибка при обновлении аватара.');
       }
     } else {
-      toast.warn("Вы не выбрали файл!"); 
+      toast.warn('Вы не выбрали файл!');
     }
   };
 
@@ -56,7 +58,7 @@ export default function Profile() {
         'http://localhost:8000/api/avatar/delete'
       );
       console.log(response.data);
-      toast.success("Аватар успешно удален!"); 
+      toast.success('Аватар успешно удален!');
     } catch (error) {
       console.error(error);
     }
@@ -120,7 +122,11 @@ export default function Profile() {
                         onChange={handleFileChange}
                       ></Input>
                       <div className='flex flex-row justify-between mt-6'>
-                        <Button onClick={handleDelete} className='w-32 flex' variant={'destructive'}>
+                        <Button
+                          onClick={handleDelete}
+                          className='w-32 flex'
+                          variant={'destructive'}
+                        >
                           Удалить
                         </Button>
                         <Button onClick={handleUpload} className='w-32 flex '>
@@ -143,13 +149,11 @@ export default function Profile() {
               <Button className='w-full md:w-fit'>Персонализация</Button>
             </div>
           </div>
-          <div className='p-10 md:mx-16 text-iney'>
+          <div className='p-10 md:mx-16'>
             <div className='text-center w-full bg-slate-500 mb-2 p-10 rounded-lg duration-500 hover:scale-110'>
-              <p>Lorem, ipsum dolor.</p>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Voluptatem, alias!
-              </p>
+              <h1 className='text-2xl font-bold'>
+                Перейдите по баннеру для покупки донатика :)
+              </h1>
             </div>
             <div className='text-center w-full bg-slate-900 p-10 rounded-lg duration-500 hover:scale-110'>
               <p>Lorem, ipsum dolor.</p>
