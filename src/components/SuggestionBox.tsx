cba0dd1a5a328e220d9334bc9c8355e5d4d8c06c
Z1ppzy <mietofff@gmail.com>
2024-04-05 19:07:34 +0300

    added logic for button SuggestionBox

--- src/components/SuggestionBox.tsx
+++ src/components/SuggestionBox.tsx
@@ -1,11 +1,30 @@
+import { ChangeEvent, FormEvent, useState } from 'react';
 import { Button } from './ui/button';
 import { Textarea } from './ui/textarea';
+import { toast } from 'react-toastify';
 
 export default function SuggestionBox() {
+  const [text, setText] = useState<string>('');
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    if (text === '') {
+      toast.warn('Пожалуйста, заполните содержимое');
+    } else {
+      toast.success('Успешно отправлено');
+    }
+  };
+
+  const handleChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
+    setText(event.target.value);
+  };
   return (
     <div className='px-10 py-10 md:px-32'>
       <div className='md:px-80'>
-        <form className='flex flex-col items-center gap-6'>
+        <form
+          className='flex flex-col items-center gap-6'
+          onSubmit={handleSubmit}
+        >
           <div className='text-center'>
             <h1 className='text-center font-bold text-2xl'>
               Предложить идею по улучшению сервера
@@ -14,8 +33,14 @@ export default function SuggestionBox() {
               Идея должна быть адекватной и правильно выраженной!
             </p>
           </div>
-          <Textarea placeholder='Снять зипзи с админа...' />
-          <Button className='w-fit'>Отправить</Button>
+          <Textarea
+            placeholder='Снять зипзи с админа...'
+            value={text}
+            onChange={handleChange}
+          />
+          <Button className='w-fit' type='submit'>
+            Отправить
+          </Button>
         </form>
       </div>
     </div>
