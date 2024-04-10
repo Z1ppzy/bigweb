9111af8744c77348bd6853608020cbea8384a1ae
Z1ppzy <mietofff@gmail.com>
2024-04-10 03:18:41 +0300

    update page NotFoundPage

--- src/Pages/NotFoundPage.tsx
+++ src/Pages/NotFoundPage.tsx
@@ -1,9 +1,30 @@
-import { Link } from 'react-router-dom';
-
+import { Button } from '@/components/ui/button';
+import { useNavigate } from 'react-router-dom';
 
 export default function NotFoundPage() {
-  return <div className='bg-gradient-to-r from-violet-500 to-fuchsia-500 h-screen flex flex-col items-center justify-center'>
-    <span className='text-white font-bold'>Страница не найдена!</span>
-    <Link to='/' className='flex flex-col font-semibold bg-white text-black py-4 px-8 rounded-lg'>Вернуться на главную</Link>
-  </div>;
+  const navigate = useNavigate();
+
+  const navigateToHome = () => {
+    navigate('/'); // Если предполагается перенаправление на страницу регистрации, убедитесь, что это корректно. Возможно, вы хотели использовать '/' для главной страницы.
+  };
+
+  return (
+    <div className='bg-gradient-to-r from-violet-500 to-fuchsia-500 h-screen flex flex-col items-center justify-center px-4 lg:px-8'>
+      <div className='text-center flex flex-col items-center'>
+        <span className='text-white font-bold text-xl md:text-2xl'>
+          Упс! Кажется, вы заблудились.
+        </span>
+        <span className='text-white font-medium text-md sm:text-lg md:text-xl mb-3'>
+          Страница, которую вы ищете, не существует или была перемещена.
+        </span>
+        <Button
+          onClick={navigateToHome}
+          variant={'secondary'}
+          className='py-2 px-4  sm:py-3 md:px-8 md:py-3 font-medium text-md sm:text-lg'
+        >
+          Перейти на главную
+        </Button>
+      </div>
+    </div>
+  );
 }
