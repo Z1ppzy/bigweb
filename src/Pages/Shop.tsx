f24b7cacd710f209baa4609c0fc10f02fadcdbf8
Z1ppzy <mietofff@gmail.com>
2024-05-01 04:41:26 +0300

    upd shop

--- src/Pages/Shop.tsx
+++ src/Pages/Shop.tsx
@@ -1,3 +1,3 @@
 export default function Shop() {
-  return <div className='text-center font-bold text-2xl p-10'>Shop</div>;
+  return <div className='text-center font-bold text-2xl p-10'>Shop2222</div>;
 }
