7b0a2dcc4e5ef8b1680d7c23d35ed197c9fa1423
Z1ppzy <mietofff@gmail.com>
2024-05-15 01:17:37 +0300

    added requirements for ConfirmEmail

--- src/components/Profile/ConfirmEmail.tsx
+++ src/components/Profile/ConfirmEmail.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import { Button } from '@/components/ui/button';
 import {
   Card,
@@ -9,10 +10,30 @@ import {
 } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function ConfirmEmail() {
+  const [email, setEmail] = useState('');
+
+  const validateEmail = (email: String) => {
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return re.test(String(email).toLowerCase());
+  };
+
+  const handleSubmit = () => {
+    if (!email) {
+      toast.error('Введите почту');
+    } else if (!validateEmail(email)) {
+      toast.error('Введите действительную почту');
+    } else {
+      toast.success('Дальнейшие действия были отправлены в письме');
+    }
+  };
+
   return (
     <div>
+      <ToastContainer />
       <Card>
         <CardHeader>
           <CardTitle>Подтвердить электронную почту</CardTitle>
@@ -23,11 +44,16 @@ export default function ConfirmEmail() {
         <CardContent className='space-y-2'>
           <div className='space-y-1'>
             <Label htmlFor='email'>Введите актуальную почту</Label>
-            <Input id='email' type='email' />
-          </div>                                                            
+            <Input
+              id='email'
+              type='email'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
         </CardContent>
         <CardFooter>
-          <Button>Подтвердить почту</Button>
+          <Button onClick={handleSubmit}>Подтвердить почту</Button>
         </CardFooter>
       </Card>
     </div>
